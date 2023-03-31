db13d95a593c74c59ec54c72750fb18499a9fe6f
Dimitrije Pesic <dimitrije.pesic@protonmail.com>
2023-03-31 11:54:35 +0200

    Initial Commit

---
+++ Finn x Dimitrije/Finn_x_DimitrijeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Finn_x_DimitrijeApp.swift
+//  Finn x Dimitrije
+//
+//  Created by Dimitrije Pesic on 31/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Finn_x_DimitrijeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
